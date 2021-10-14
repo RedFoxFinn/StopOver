3cf9a79b90084d4db0d006b398cd618e577c16df
RedFoxFinn <laborantti.antti@gmail.com>
2021-10-14 18:13:11 +0300

    test: test building: transportmodes

--- src/components/transportmodes.spec.js
+++ src/components/transportmodes.spec.js
@@ -10,13 +10,13 @@ import { Transportmodes } from './transportmodes';
 
 describe('transportmodes - unit tests', () => {
   let dummy = false;
+  beforeEach(() => render(<Provider store={store}><Transportmodes id='stopover.unit.test' /></Provider>));
   it('dummy', () => {
     expect(dummy).toBe(false);
     dummy = true;
     expect(dummy).toBe(true);
   });
-  it('InputModule renders', () => {
-    render(<Provider store={store}><Transportmodes id='stopover.unit.test' /></Provider>);
+  it('transportmodes renders', () => {
     const component = screen.queryByTestId('stopover.unit.test');
     expect(component).toBeTruthy();
     isCompositeComponentWithType(component, Transportmodes);
@@ -31,38 +31,176 @@ describe('transportmodes - unit tests', () => {
     expect(component.textContent).toMatch('Ratikka');
     expect(component.textContent).toMatch('Kävely');
   });
-});
-
-describe('transportmodes - integration tests', () => {
-  let dummy = false;
-  it('dummy', () => {
-    expect(dummy).toBe(false);
-    dummy = true;
-    expect(dummy).toBe(true);
+  it('transportmodes - bicycle', () => {
+    const bicycle = screen.queryByTestId('stopover.unit.test.bicycle');
+    expect(bicycle).toBeTruthy();
+    isCompositeComponentWithType(bicycle, ToggleButton);
+    let state_preferences = store.getState().preferences;
+    expect(state_preferences.all).toBe(true);
+    expect(state_preferences.bicycle).toBe(false);
+    act(() => userEvent.click(bicycle));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(false);
+      expect(state_preferences.bicycle).toBe(true);
+    }, 500);
+    act(() => userEvent.click(bicycle));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(true);
+      expect(state_preferences.bicycle).toBe(false);
+    }, 500);
   });
-  it('transportmodes - state', () => {
-    render(<Provider store={store}><Transportmodes id='stopover.integration.test' /></Provider>);
-    const component = screen.queryByTestId('stopover.integration.test');
-    expect(component).toBeTruthy();
-    isCompositeComponentWithType(component, Transportmodes);
-    let preferences = store.getState().preferences;
-    expect(preferences.all).toBe(true);
-    expect(preferences.bicycle).toBe(false);
-    expect(preferences.bus).toBe(false);
-    expect(preferences.ferry).toBe(false);
-    expect(preferences.metro).toBe(false);
-    expect(preferences.tram).toBe(false);
-    expect(preferences.rail).toBe(false);
-    expect(preferences.walk).toBe(false);
-    const all = within(component).getByText('Kaikki kulkukeinot');
-    const bicycle = within(component).getByText('Pyörä');
-    const car = within(component).getByText('Auto');
-    const bus = within(component).getByText('Bussi');
-    const ferry = within(component).getByText('Lautta');
-    const metro = within(component).getByText('Metro');
-    const rail = within(component).getByText('Juna');
-    const tram = within(component).getByText('Ratikka');
-    const walk = within(component).getByText('Kävely');
+  it('transportmodes - bus', () => {
+    const bus = screen.queryByTestId('stopover.unit.test.bus');
+    expect(bus).toBeTruthy();
+    isCompositeComponentWithType(bus, ToggleButton);
+    let state_preferences = store.getState().preferences;
+    expect(state_preferences.all).toBe(true);
+    expect(state_preferences.bus).toBe(false);
+    act(() => userEvent.click(bus));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(false);
+      expect(state_preferences.bus).toBe(true);
+    }, 500);
+    act(() => userEvent.click(bus));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(true);
+      expect(state_preferences.bus).toBe(false);
+    }, 500);
+  });
+  it('transportmodes - car', () => {
+    const car = screen.queryByTestId('stopover.unit.test.car');
+    expect(car).toBeTruthy();
+    isCompositeComponentWithType(car, ToggleButton);
+    let state_preferences = store.getState().preferences;
+    expect(state_preferences.all).toBe(true);
+    expect(state_preferences.car).toBe(false);
+    act(() => userEvent.click(car));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(false);
+      expect(state_preferences.car).toBe(true);
+    }, 500);
+    act(() => userEvent.click(car));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(true);
+      expect(state_preferences.car).toBe(false);
+    }, 500);
+  });
+  it('transportmodes - ferry', () => {
+    const ferry = screen.queryByTestId('stopover.unit.test.ferry');
+    expect(ferry).toBeTruthy();
+    isCompositeComponentWithType(ferry, ToggleButton);
+    let state_preferences = store.getState().preferences;
+    expect(state_preferences.all).toBe(true);
+    expect(state_preferences.ferry).toBe(false);
+    act(() => userEvent.click(ferry));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(false);
+      expect(state_preferences.ferry).toBe(true);
+    }, 500);
+    act(() => userEvent.click(ferry));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(true);
+      expect(state_preferences.ferry).toBe(false);
+    }, 500);
+  });
+  it('transportmodes - metro', () => {
+    const metro = screen.queryByTestId('stopover.unit.test.subway');
+    expect(metro).toBeTruthy();
+    isCompositeComponentWithType(metro, ToggleButton);
+    let state_preferences = store.getState().preferences;
+    expect(state_preferences.all).toBe(true);
+    expect(state_preferences.subway).toBe(false);
+    act(() => userEvent.click(metro));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(false);
+      expect(state_preferences.subway).toBe(true);
+    }, 500);
+    act(() => userEvent.click(metro));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(true);
+      expect(state_preferences.subway).toBe(false);
+    }, 500);
+  });
+  it('transportmodes - rail', () => {
+    const rail = screen.queryByTestId('stopover.unit.test.rail');
+    expect(rail).toBeTruthy();
+    isCompositeComponentWithType(rail, ToggleButton);
+    let state_preferences = store.getState().preferences;
+    expect(state_preferences.all).toBe(true);
+    expect(state_preferences.rail).toBe(false);
+    act(() => userEvent.click(rail));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(false);
+      expect(state_preferences.rail).toBe(true);
+    }, 500);
+    act(() => userEvent.click(rail));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(true);
+      expect(state_preferences.rail).toBe(false);
+    }, 500);
+  });
+  it('transportmodes - tram', () => {
+    const tram = screen.queryByTestId('stopover.unit.test.tram');
+    expect(tram).toBeTruthy();
+    isCompositeComponentWithType(tram, ToggleButton);
+    let state_preferences = store.getState().preferences;
+    expect(state_preferences.all).toBe(true);
+    expect(state_preferences.tram).toBe(false);
+    act(() => userEvent.click(tram));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(false);
+      expect(state_preferences.tram).toBe(true);
+    }, 500);
+    act(() => userEvent.click(tram));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(true);
+      expect(state_preferences.tram).toBe(false);
+    }, 500);
+  });
+  it('transportmodes - walk', () => {
+    const walk = screen.queryByTestId('stopover.unit.test.walk');
+    expect(walk).toBeTruthy();
+    isCompositeComponentWithType(walk, ToggleButton);
+    let state_preferences = store.getState().preferences;
+    expect(state_preferences.all).toBe(true);
+    expect(state_preferences.walk).toBe(false);
+    act(() => userEvent.click(walk));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(false);
+      expect(state_preferences.walk).toBe(true);
+    }, 500);
+    act(() => userEvent.click(walk));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(true);
+      expect(state_preferences.walk).toBe(false);
+    }, 500);
+  });
+  it('transportmodes - all', () => {
+    const all = screen.queryByTestId('stopover.unit.test.all');
+    const bicycle = screen.queryByTestId('stopover.unit.test.bicycle');
+    const car = screen.queryByTestId('stopover.unit.test.car');
+    const bus = screen.queryByTestId('stopover.unit.test.bus');
+    const ferry = screen.queryByTestId('stopover.unit.test.ferry');
+    const metro = screen.queryByTestId('stopover.unit.test.subway');
+    const rail = screen.queryByTestId('stopover.unit.test.rail');
+    const tram = screen.queryByTestId('stopover.unit.test.tram');
+    const walk = screen.queryByTestId('stopover.unit.test.walk');
     expect(all).toBeTruthy();
     expect(bicycle).toBeTruthy();
     expect(bus).toBeTruthy();
@@ -81,10 +219,50 @@ describe('transportmodes - integration tests', () => {
     isCompositeComponentWithType(tram, ToggleButton);
     isCompositeComponentWithType(rail, ToggleButton);
     isCompositeComponentWithType(walk, ToggleButton);
-    
+    let state_preferences = store.getState().preferences;
+    expect(state_preferences.all).toBe(true);
+    expect(state_preferences.bicycle).toBe(false);
+    expect(state_preferences.car).toBe(false);
+    expect(state_preferences.bus).toBe(false);
+    expect(state_preferences.ferry).toBe(false);
+    expect(state_preferences.subway).toBe(false);
+    expect(state_preferences.tram).toBe(false);
+    expect(state_preferences.rail).toBe(false);
+    expect(state_preferences.walk).toBe(false);
     act(() => {
+      userEvent.click(bicycle);
+      userEvent.click(bus);
+      userEvent.click(car);
+      userEvent.click(ferry);
+      userEvent.click(rail);
+      userEvent.click(tram);
+      userEvent.click(metro);
+      userEvent.click(walk);
     });
+    state_preferences = store.getState().preferences;
     setTimeout(() => {
-    }, 3500);
+      expect(state_preferences.all).toBe(false);
+      expect(state_preferences.bicycle).toBe(true);
+      expect(state_preferences.bus).toBe(true);
+      expect(state_preferences.car).toBe(true);
+      expect(state_preferences.ferry).toBe(true);
+      expect(state_preferences.subway).toBe(true);
+      expect(state_preferences.tram).toBe(true);
+      expect(state_preferences.rail).toBe(true);
+      expect(state_preferences.walk).toBe(true);
+    }, 500);
+    act(() => userEvent.click(all));
+    state_preferences = store.getState().preferences;
+    setTimeout(() => {
+      expect(state_preferences.all).toBe(true);
+      expect(state_preferences.bicycle).toBe(false);
+      expect(state_preferences.bus).toBe(false);
+      expect(state_preferences.car).toBe(false);
+      expect(state_preferences.ferry).toBe(false);
+      expect(state_preferences.subway).toBe(false);
+      expect(state_preferences.tram).toBe(false);
+      expect(state_preferences.rail).toBe(false);
+      expect(state_preferences.walk).toBe(false);
+    }, 500);
   });
 });
